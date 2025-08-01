cffcb54a6350c82ba90a050484558a178288e283
FarrelArdam <farrel_ardam_34rpl@student.smktelkom-mlg.sch.id>
2025-08-01 14:11:14 +0700

    Add files via upload

---
+++ KelasXR5.java
@@ -0,0 +1,10 @@
+public class KelasXR5 {
+    public static void main(String[] args) {
+        System.out.println("Farrel Maulana Habieb Aprilo Ardam");
+        System.out.println("MTsN 1 Malang");
+        System.out.println("Turen Kab.Malang");
+        System.out.println("Bermain Bola Basket");
+        System.out.println("Menjadi Pemain Basket Atau Programmer");
+        System.out.println("15");
+    }
+}
